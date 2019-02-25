eca8e7b9cdaf247b92b7f7a13c8a9e438a30479b
suryaa thiagarajan <47815224+suryaathiagu@users.noreply.github.com>
2019-02-25 12:04:28 +0530

    Add files via upload

---
+++ s1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+ 
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		StringBuffer sb=new StringBuffer(a);
+		System.out.println(sb.reverse());
+	}
+}
