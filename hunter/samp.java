d7f7975e2cd4e6a5091742c9f45cc34bd0e90302
suryaa thiagarajan <47815224+suryaathiagu@users.noreply.github.com>
2019-06-28 21:57:54 +0530

    number which are equal to the index value

---
+++ hunter/samp.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class samp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=s.nextInt();
+		}
+		//List<Integer> al=new ArrayList<>();
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]==i)
+			{
+				if(i!=a.length-1)
+				System.out.print(i+" ");
+				else
+					System.out.print(i);
+			}
+				
+		}
+
+	}
+}
